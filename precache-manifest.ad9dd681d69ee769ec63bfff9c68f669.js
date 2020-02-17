bebd76167f4a61127ab1df2de8dc57f4ad6f4369
Косарев Артем <kosarev.web@mail.ru>
2020-02-17 14:22:01 +0500

    Updates

---
+++ precache-manifest.ad9dd681d69ee769ec63bfff9c68f669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd1a6dcc70dd1f9f3a42ad8fd77df01",
+    "url": "/NewProject_SocialNetwork_React/index.html"
+  },
+  {
+    "revision": "30258d714e61beaa38bd",
+    "url": "/NewProject_SocialNetwork_React/static/css/main.19cc91c9.chunk.css"
+  },
+  {
+    "revision": "efcbc7db2a5ebd53eab1",
+    "url": "/NewProject_SocialNetwork_React/static/js/2.90cf0755.chunk.js"
+  },
+  {
+    "revision": "efd41bba8700f842987b229e0d7022f1",
+    "url": "/NewProject_SocialNetwork_React/static/js/2.90cf0755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30258d714e61beaa38bd",
+    "url": "/NewProject_SocialNetwork_React/static/js/main.591f01e4.chunk.js"
+  },
+  {
+    "revision": "4804b3c3e69deb2f8a90",
+    "url": "/NewProject_SocialNetwork_React/static/js/runtime-main.7b103e14.js"
+  }
+]);
