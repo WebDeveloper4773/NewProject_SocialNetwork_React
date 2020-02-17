f860a8741a4207eea918792304665779b4c32c02
Косарев Артем <kosarev.web@mail.ru>
2020-02-17 15:24:32 +0500

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,15 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/redux-store';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {Provider} from "react-redux";
 
 let rerenderApp = (store) => {
-    ReactDOM.render(<BrowserRouter>
+    ReactDOM.render(<HashRouter>
                         <Provider store={store}>
                             <App />
                         </Provider>
-                    </BrowserRouter>, document.getElementById('root'));
+                    </HashRouter>, document.getElementById('root'));
 }
 
 rerenderApp(store);
