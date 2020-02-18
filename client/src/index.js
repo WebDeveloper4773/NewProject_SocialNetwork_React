6e99058f9a555ae950fa97c5838e771b6f0a7de8
Косарев Артем <kosarev.web@mail.ru>
2020-02-18 15:38:14 +0500

    hashrouter

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/redux-store';
 import App from './App';
-import {BrowserRouterб HashRouter } from 'react-router-dom';
+import {BrowserRouter, HashRouter } from 'react-router-dom';
 import {Provider} from "react-redux";
 
 let rerenderApp = (store) => {
